c231f81469c1cbc1004f71bd5771eac2547bb8f6
gaokai <gkaigk1987@gmail.com>
2019-12-07 13:37:42 +0800

    增加Spring Cloud Gateway

---
+++ gateway/src/main/java/com/gk/cloud/alibaba/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.gk.cloud.alibaba.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description: TODO
+ * @Author: GK
+ * @Date: 2019/12/7
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
