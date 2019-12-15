ea2571bf94a47aabe50e80e7e5cbf732ed5ba04f
GaoKai <gkaigk1987@gmail.com>
2019-12-15 20:27:16 +0800

    增加sentinel配置，待进一步完善，测试有问题，没有fallback

--- business/business-user-service/src/main/java/com/gk/cloud/alibaba/business/controller/fallback/UserServiceFallback.java
+++ business/business-user-service/src/main/java/com/gk/cloud/alibaba/business/controller/fallback/UserServiceFallback.java
@@ -1,7 +1,8 @@
 package com.gk.cloud.alibaba.business.controller.fallback;
 
 import com.gk.cloud.alibaba.commons.model.User;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -10,8 +11,10 @@ import java.util.List;
  * @Author: GK
  * @Date: 2019/12/10
  */
-@Slf4j
+//@Slf4j
 public class UserServiceFallback {
+
+    private static  final Logger log = LoggerFactory.getLogger(UserServiceFallback.class);
 
     /**
      * 熔断方法
